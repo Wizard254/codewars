1484192a13d1fd08809b51d2136f377084bb8a95
Nicholas Ajwang' Anyona <45289389+Wizard254@users.noreply.github.com>
2024-04-18 19:49:49 +0300

    Create main.go

---
+++ dont-give-me-five/main.go
@@ -0,0 +1,32 @@
+package kata
+
+func DontGiveMeFive(start int, end int) int {
+  if start < 0 && end < 0 {
+    // Handle both start and end are negatives
+    // -4 -3 -2 -1 
+    return DontGiveMeFivePositives(-end, -start)
+  } else if start < 0 {
+    // Handle case when start is negative
+    // -3 -2 -1 0 1 2 3
+    return DontGiveMeFivePositives(1, -start) + DontGiveMeFivePositives(1, end) + 1
+  }
+  
+  return DontGiveMeFivePositives(start, end)
+}
+
+// DontGiveMeFivePositives same as DontGiveMeFive but only handles positive values
+func DontGiveMeFivePositives(start, end int) int {
+  // 5 15 25 35 45
+  // 1  3  5  7  9
+  count := (end - start) + 1
+  return count - (HowMany5(end) - HowMany5(start))
+}
+
+// HowMany5 Given an `end` value, returns the count of numbers with 5 up to and including `end`
+func HowMany5(end int) int {
+  odd := end / 5
+  n := (odd + 1) / 2
+  return n
+}
+
+
